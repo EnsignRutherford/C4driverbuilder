a38f5bb16452bffbe0f411e7e9555d0dd0de2240
EnsignRutherford <31388235+EnsignRutherford@users.noreply.github.com>
2022-01-04 09:53:01 -0500

    Update build.bat
    If no image name is specified the batch file will loop through all the files in the current folder that aren't 'selected' and create the driver.

--- build.bat
+++ build.bat
@@ -3,8 +3,12 @@ if "%1"=="" goto have_0
 if "%2"=="" goto have_1
 goto end
 :have_0
-echo ERROR: Missing image file name for driver creation.
+for /f "delims=" %%i in ('dir /b *.png ^| findstr /v selected') do (
+echo Creating driver uibutton_%%~ni.c4z
+python c4driver.py %%~ni
+)
 goto end
 :have_1
+echo Creating driver uibutton_%1.c4z
 python c4driver.py %1
 :end
