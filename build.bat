bcf47558a5d37594d6eb78085d6ee4b33cbfe44f
EnsignRutherford <31388235+EnsignRutherford@users.noreply.github.com>
2022-01-05 08:13:08 -0500

    Update build.bat
    Removed status messages to be handled by the python script.

--- build.bat
+++ build.bat
@@ -17,11 +17,9 @@ if "%2"=="" goto have_1
 goto end
 :have_0
 for /f "delims=" %%i in ('dir /b *.png ^| findstr /v selected') do (
-echo Creating driver uibutton_%%~ni.c4z
-python c4driver.py %%~ni
+  python c4driver.py %%~ni
 )
 goto end
 :have_1
-echo Creating driver uibutton_%1.c4z
 python c4driver.py %1
 :end
